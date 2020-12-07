b1181422328dbcc7f0b1874febe1d54a7bd49241
kongchuiliang <626100676@qq.com>
2020-12-07 20:04:32 +0800

    feat: 添加文档服务器

---
+++ docs/.vuepress/server.js
@@ -0,0 +1,12 @@
+const path = require('path')
+const Koa = require('koa')
+const serve = require('koa-static')
+const app = new Koa()
+
+app.use(
+  serve(path.resolve(__dirname, 'dist'), {
+    maxage: 30000
+  })
+)
+
+app.listen(6000)
---
+++ webhook.js
@@ -0,0 +1,10 @@
+const { spawnSync } = require('child_process')
+const WebHook = require('webhook').default
+
+new WebHook({
+  onTagEvent() {
+    spawnSync('git', ['checkout', 'master'])
+    spawnSync('git', ['pull'])
+    spawnSync('npm', ['run', 'docs:build'])
+  }
+}).listen(6001)
