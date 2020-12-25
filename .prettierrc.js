4f7d0ed70404b0d2d569078e7e1b4521641c254a
kongchuiliang <626100676@qq.com>
2020-12-25 15:14:17 +0800

    fix: 修改settings.json配置,解决保存时卡死,不能自动修复代码格式的问题

--- .prettierrc.js
+++ .prettierrc.js
@@ -5,7 +5,7 @@ module.exports = {
   singleQuote: true,
   // 对象/数组最后一个元素不添加逗号
   trailingComma: 'none',
-  // 单行最大宽度
+  // 单行最大字节宽度，英文字符占一个字节，中文通常占2个字节
   printWidth: 120,
   // 箭头函数只有一个参数时不使用括号
   arrowParens: 'avoid',
